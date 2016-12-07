dae98c921fa6514c2aee6252f2311d1b7fa7078c
git-grav <brunajunkemail@gmail.com>
2016-12-07 18:46:04 -0500

    Adding plugins
    bootstrap, bootstrapped, analytics

--- cache/compiled/files/802ad8b6ba6b0f9434a3077da5a70fc4.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Applications/MAMP/htdocs/git-grav/grav/user/config/media.yaml',
-    'modified' => 1481153967,
-    'data' => [
-        
-    ]
-];
